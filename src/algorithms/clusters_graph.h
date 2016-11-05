6d796c71ad595f78ed7521dfb377b653709e8f30
Clémence Réda <clmence.reda@ens-cachan.fr>
2016-11-05 13:01:03 +0100

    v0.5.4 [graph algorithms] construction du blobsGraph

---
+++ src/algorithms/clusters_graph.h
@@ -0,0 +1,15 @@
+#include <models/cluster.h>
+#include <models/agent.h>
+#include <graphs/matrix.h>
+#include <distances/cluster2cluster.h>
+#include <distances/obj2obj.h>
+
+// @k est le nombre de clusters
+Matrix get_clustersGraph(vector<cluster>clusters, int k, agent a) {
+    Matrix<cl2cl, obj2obj> m;
+    int i;
+    for(i = 0; i < k; ++i) {
+	m.add(clusters[i]);
+    };
+    return (m);
+};
